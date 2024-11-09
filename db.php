59be590bdb30d6954a4765b584aa6f584fe247c4
manasvi111 <122574665+manasvi111@users.noreply.github.com>
2024-11-09 11:22:46 -0800

    Fix the username and password in the database file

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "n0203814_n0203814"; // Default XAMPP MySQL username
-$password = "Manasvi123"; // Leave blank for XAMPP
-$dbname = "n0203814_raami_db"; // The database you created earlier
+$username = "root"; // Default XAMPP MySQL username
+$password = ""; // Leave blank for XAMPP
+$dbname = "raami_db"; // The database you created earlier
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <a href="index.php" class="logo">Raami</a> <!-- Link to home page -->
         <nav class="d-flex flex-grow-1 justify-content-center">
             <a href="#vendors" class="nav-link">Vendor directory</a>
-            <a href="#features" class="nav-link">Services</a>
+            <a href="#features" class="nav-link">Serv----ices</a>
             <a href="resources.php" class="nav-link">Resources</a>
             <a href="about.php" class="nav-link">About Us</a>
             <a href="contact.php" class="nav-link">Contact Us</a>
