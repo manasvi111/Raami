5cfd8644ef05e362f93b566dbe429319efaee06e
manasvi111 <122574665+manasvi111@users.noreply.github.com>
2024-11-29 19:15:22 -0800

    Update the connection to run it on localhost

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php /*
+<?php 
 $servername = "localhost";
 $username = "root"; // Default XAMPP MySQL username
 $password = ""; // Leave blank for XAMPP
@@ -11,19 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>*/
-
-
-$servername = "localhost";
-$username = "n0203814_n0203814"; // Default XAMPP MySQL username
-$password = "Manasvi123"; // Leave blank for XAMPP
-$dbname = "n0203814_raami_db"; // The database you created earlier
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
+
+
+ 
